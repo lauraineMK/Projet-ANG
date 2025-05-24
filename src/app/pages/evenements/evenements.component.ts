8b1842be7854bb7d493393f516d94c199cecd4aa
LauraineMk <123209759+lauraineMK@users.noreply.github.com>
2025-05-24 20:05:49 +0200

    ajout

--- src/app/pages/evenements/evenements.component.ts
+++ src/app/pages/evenements/evenements.component.ts
@@ -1,11 +1,89 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-evenements',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './evenements.component.html',
-  styleUrl: './evenements.component.css'
+  styleUrls: ['./evenements.component.css']
 })
 export class EvenementsComponent {
+  evenements: {
+  nom: string;
+  periode: string;
+  description: string;
+  image: string;
+  commentaires: string[];
+}[] = [
+  {
+    nom: 'Révolution française',
+    periode: '1789-1799',
+    description: 'Un moment fondamental dans l’histoire de France.',
+    image: 'https://m.media-amazon.com/images/I/71k7GwLWjpL._AC_UF1000,1000_QL80_.jpg',
+    commentaires: []
+  },
+    {
+        nom: 'Première Guerre mondiale',
+        periode: '1914-1918',
+        description: 'Un conflit mondial majeur ayant bouleversé le XXe siècle.',
+        image: 'https://m.media-amazon.com/images/I/915v6CT-rqL._AC_UF1000,1000_QL80_.jpg',
+        commentaires: []
+         }
+];
 
+ 
+
+
+  nouveauxCommentaires: string[] = this.evenements.map(() => '');
+
+  nouvelEvenement = {
+    nom: '',
+    periode: '',
+    description: '',
+    image: '',
+  };
+
+  imagePreview: string = '';
+
+  ajouterCommentaire(index: number): void {
+    const commentaire = this.nouveauxCommentaires[index].trim();
+    if (commentaire) {
+      this.evenements[index].commentaires.push(commentaire);
+      this.nouveauxCommentaires[index] = '';
+    }
+  }
+
+  onImageSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+        this.nouvelEvenement.image = this.imagePreview;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  ajouterEvenement(): void {
+    if (
+      this.nouvelEvenement.nom &&
+      this.nouvelEvenement.periode &&
+      this.nouvelEvenement.description &&
+      this.nouvelEvenement.image
+    ) {
+      this.evenements.push({
+        ...this.nouvelEvenement,
+        commentaires: []
+      });
+      this.nouvelEvenement = { nom: '', periode: '', description: '', image: '' };
+      this.imagePreview = '';
+      this.nouveauxCommentaires.push('');
+    } else {
+      alert("Tous les champs sont obligatoires.");
+    }
+  }
 }
